9f40af198071720af9edf44ef22c351ee4c8da06
Timothy Gu <timothygu99@gmail.com>
2015-02-11 22:12:22 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var ejs = require('ejs');
+var fs  = require('fs');
+
+exports.name = 'ejs';
+exports.outputFormat = 'html';
+exports.compile = ejs.compile;
+exports.compileClient = function (source, options) {
+  options = options || {};
+  options.client = true;
+  return exports.compile(source, options).toString();
+};
+exports.compileFile = function (path, options) {
+  options = options || {};
+  options.filename = options.filename || path;
+  return exports.compile(fs.readFileSync(path, 'utf8'), options);
+};
+exports.compileFileClient = function (path, options) {
+  options = options || {};
+  options.client = true;
+  options.filename = options.filename || path;
+  return exports.compile(fs.readFileSync(path, 'utf8'), options).toString();
+};
