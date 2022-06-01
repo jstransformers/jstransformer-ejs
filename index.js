708028371177e18dc1566b7eeff46075b11fe5cb
Kevin Van Lierde <kevin.van.lierde@gmail.com>
2022-06-01 02:24:20 +0200

    Adds support for methods: compileFileAsync, render, renderFile, renderFileAsync

--- index.js
+++ index.js
@@ -3,17 +3,58 @@
 const fs = require('fs')
 const ejs = require('ejs')
 
-exports.name = 'ejs'
-exports.outputFormat = 'html'
-exports.compile = ejs.compile
-exports.compileClient = function (source, options) {
-  options = options || {}
-  options.client = true
-  return exports.compile(source, options).toString()
+const transformer = {
+  name: 'ejs',
+  outputFormat: 'html',
 }
 
-exports.compileFile = function (path, options) {
+transformer.compile = function (source, options) {
+  return ejs.compile(source, { ...options, async: false, client: false })
+}
+
+transformer.compileClient = function (source, options) {
+  return ejs.compile(source, { ...options, async: false, client: true }).toString()
+}
+
+transformer.compileFile = function (path, options) {
   options = options || {}
   options.filename = options.filename || path
-  return exports.compile(fs.readFileSync(path, 'utf8'), options)
+  return transformer.compile(fs.readFileSync(path, 'utf8'), options)
 }
+
+transformer.compileFileAsync = function (path, options) {
+  options = options || {}
+  options.filename = options.filename || path
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, source) => {
+      if (error) {
+        reject(error)
+      }
+
+      resolve(transformer.compile(source, options))
+    })
+  })
+}
+
+transformer.render = function (source, options, locals) {
+  return ejs.render(source, locals, { ...options, async: false })
+}
+
+transformer.renderFile = function (path, options, locals = null) {
+  options.filename = options.filename || path
+  return ejs.render(fs.readFileSync(path, 'utf8'), locals, { ...options, async: false })
+}
+
+transformer.renderFileAsync = function (path, options, locals = null) {
+  return new Promise((resolve, reject) => {
+    ejs.renderFile(path, locals, options, (error, result) => {
+      if (error) {
+        reject(error)
+      }
+
+      resolve(result)
+    })
+  })
+}
+
+module.exports = transformer
